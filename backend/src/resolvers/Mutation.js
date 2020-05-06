7843f455730ff46781fcd2feda2e450f479c13f6
diegolealb <diegolealb@gmail.com>
2020-05-06 17:02:14 -0500

    Worked on Update and delete features

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -8,6 +8,20 @@ const Mutations = {
         console.log(item);
 
         return item;
+    },
+    updateItem(parent, args, ctx, info) {
+        // Take a copy of the updates and remove id from update
+        const updates = {...args};
+        delete updates.id;
+        return ctx.db.mutation.updateItem({
+            data: { ...updates },
+            where: { id: args.id },
+        }, info)
+    },
+    async deleteItem(parent, args, ctx, info) {
+        const where = { id: args.id };
+        const item = await ctx.db.query.item({ where }, `{ id title }`);
+        return ctx.db.mutation.deleteItem({ where }, info);
     }
 };
 
