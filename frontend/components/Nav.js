f8a6a9b2b35634632b57d6d8e759eaf67b380b34
Diego Leal <diegolealb@gmail.com>
2020-05-20 17:14:39 -0500

    Adds toggleCart button on Nav

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -2,6 +2,8 @@ import Link from "next/link";
 import NavStyles from "./styles/NavStyles";
 import User from "./User";
 import Signout from "./Signout";
+import { Mutation } from "react-apollo";
+import { TOGGLE_CART_MUTATION } from "./Cart";
 
 const Nav = () => (
   <User>
@@ -21,6 +23,9 @@ const Nav = () => (
             <Link href="/me">
               <a>Account</a>
             </Link>
+            <Mutation mutation={TOGGLE_CART_MUTATION}>
+              {(toggleCart) => <button onClick={toggleCart}>My Cart</button>}
+            </Mutation>
             <Signout />
           </>
         )}
