7dda15311229423be88589ccbf3007ca10926a3e
Diego Leal <diegolealb@gmail.com>
2020-05-29 15:34:16 -0500

    Adds order query

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -26,6 +26,32 @@ const Query = {
 
     return ctx.db.query.users({}, info);
   },
+  async order(parent, args, ctx, info) {
+    // 1. Make sure user is logged in
+    const userId = ctx.request.userId;
+    if (!userId) {
+      throw new Error("You are not logged in");
+    }
+    // 2. Query the current order
+    const order = await ctx.db.query.order(
+      {
+        where: {
+          id: args.id,
+        },
+      },
+      info
+    );
+    // 3. Check if they have permissions to see this order
+    const onwsOrder = order.user.id === ctx.request.userId;
+    const hasPermissionToSeeOrder = ctx.request.user.permissions.includes(
+      "ADMIN"
+    );
+    if (!onwsOrder || !hasPermissionToSeeOrder) {
+      throw new Error("You can't see this buddy");
+    }
+    // 4. Return the order
+    return order;
+  },
 };
 
 module.exports = Query;
