5faab9d927c3fefef93ccfff2a8f0057837d853b
Diego Leal <diegolealb@gmail.com>
2020-05-29 15:34:04 -0500

    Finishes createOrder mutation

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -262,7 +262,7 @@ const Mutations = {
       cart { 
         id 
         quantity 
-        item { title price id description image }}
+        item { title price id description image largeImage }}
       }`
     );
     // 2. Recalculate the total for the price
@@ -278,9 +278,33 @@ const Mutations = {
       source: args.token,
     });
     // 4. Convert the CartItems to OrderItems
+    const orderItems = user.cart.map((cartItem) => {
+      const orderItem = {
+        ...cartItem.item,
+        quantity: cartItem.quantity,
+        user: {
+          connect: { id: userId },
+        },
+      };
+      delete orderItem.id;
+      return orderItem;
+    });
     // 5. Create the Order
+    const order = await ctx.db.mutation.createOrder({
+      data: {
+        total: charge.amount,
+        charge: charge.id,
+        items: { create: orderItems },
+        user: { connect: { id: userId } },
+      },
+    });
     // 6. Clear the users cart and delete CartItems
+    const cartItemIds = user.cart.map((cartItem) => cartItem.id);
+    await ctx.db.mutation.deleteManyCartItems({
+      where: { id_in: cartItemIds },
+    });
     // 7. Return order to client
+    return order;
   },
 };
 
